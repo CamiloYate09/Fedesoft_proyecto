ca3745d40df2eac7b3f2ccffc35e803ae476a5b3
camilo Yate <cyateya35439@universidadean.edu.co>
2018-08-18 04:13:51 -0500

    enviando a Heroku

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,47 @@
 var postgresql = require('pg');
+var bcript = require('bcryptjs'); // modulo para la encriptacion de contraseñas
 
 module.exports = {
 
     getSignUp: function (req, res, next) {
 
         return res.render('users/signup');
+
+    },
+
+// metodos
+    postSignUp: function (req, res, next) {
+        // console.log(req.body);
+        // return;
+
+        var salt = bcript.genSaltSync(10);
+        var password = bcript.hashSync(req.body.password, salt);
+
+        var user = {
+            email: body.email,
+            nombre: body.nombre,
+            apellido: body.apellido,
+            password: password,
+            cuidad: body.cuidad,
+            pais: body.pais,
+            intereses: body.interes
+
+        };
+
+        var config = require('.././database/config');
+
+        var db = postgresql.createConnection(config);
+
+        db.connect();
+
+        db.query('INSERT INTO usuarios SET ?', user, function (err, rows, fileds) {
+            if (err) throw  err;
+
+            db.end();
+
+        });
+
+        return;
 
     }
 
--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,9 @@ router.get('/', controllers.HomeController.index);
 // rutas de usuario
 router.get('/auth/signup', controllers.UserController.getSignUp);
 
+
+// ruta para el formulario
+router.post('/auth/signup', controllers.UserController.postSignUp);
+
+
 module.exports = router;
